6696383d3105063dc9df833bc74d62a53f7b2f98
Giampaolo Tranchida <giampaolo.tranchida@vd.ch>
2025-06-16 16:23:58 +0200

    Update Prisma client configuration and dependencies; upgrade postcss to version 8.5.6, switch Prisma client import path, and modify schema for client generation.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../src/lib/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "$lib/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export default prisma;
